00daf43326fd40440335505082780dca6b889d27
Jules <julesmoreno66@gmail.com>
2022-03-03 15:45:58 +0800

    add files secret life of object

---
+++ secret_life_of_object.js
@@ -0,0 +1,15 @@
+// Encapsulation
+
+let objectLiteral = {
+    1: "hello",
+    2: "world",
+}
+
+console.log(typeof (Object.keys(objectLiteral)[0]))
+
+// https://www.youtube.com/watch?v=HboT8g_QSGc&list=PL4cUxeGkcC9i5yvDkJgt60vNVWffpblB7&index=5
+
+// study abstraction in oop
+
+// session 2
+// page 112
