c85d909bde403833cbdf1aeea340abfe3597c967
Jules <julesmoreno66@gmail.com>
2022-03-07 17:04:26 +0800

    static super and extends

--- secret_life_of_object.js
+++ secret_life_of_object.js
@@ -1,110 +1,110 @@
 // Encapsulation
 
-let objectLiteral = {
-    1: "hello",
-    2: "world",
-}
+// let objectLiteral = {
+//     1: "hello",
+//     2: "world",
+// }
 
-console.log(typeof (Object.keys(objectLiteral)[0]))
+// console.log(typeof (Object.keys(objectLiteral)[0]))
 
-let userInfo = {
-    name: "Jules",
-    email: "julesmoreno@gmail.com",
-    login() {
-        console.log(this.email)
-    },
+// let userInfo = {
+//     name: "Jules",
+//     email: "julesmoreno@gmail.com",
+//     login() {
+//         console.log(this.email)
+//     },
     
-}
+// }
 
-console.log(userInfo)
-userInfo.login()
+// console.log(userInfo)
+// userInfo.login()
 
-console.log(window === this)
-console.log(this.location)
+// console.log(window === this)
+// console.log(this.location)
 
-function videos(title) {
-    this.title = title
-   return this
-}
+// function videos(title) {
+//     this.title = title
+//    return this
+// }
 
-let newItem = new videos("rebetika")
-console.log(newItem)
+// let newItem = new videos("rebetika")
+// console.log(newItem)
 
-// tricks with this
-function storage() {
-    this.sum = 0
-    this.greet = "hello"
-}
+// // tricks with this
+// function storage() {
+//     this.sum = 0
+//     this.greet = "hello"
+// }
 
-storage()
-console.log(this.greet)
+// storage()
+// console.log(this.greet)
 
-let video = {
-    title: "tiny cat",
-    tags: ["a", "b", "c"],
-    log() {
-        this.tags.forEach(element => {
-            console.log(this.title ,element)
-        })
-    },
-    info () {
-        this.tags.forEach(element => {
-            console.log(this, element)
-        }, this)
-    }
-}
+// let video = {
+//     title: "tiny cat",
+//     tags: ["a", "b", "c"],
+//     log() {
+//         this.tags.forEach(element => {
+//             console.log(this.title ,element)
+//         })
+//     },
+//     info () {
+//         this.tags.forEach(element => {
+//             console.log(this, element)
+//         }, this)
+//     }
+// }
 
-video.log()
-video.info()
+// video.log()
+// video.info()
 
-class Users {
-    constructor(name, email) {
-        this.name = name;
-        this.email = email;
-    }
-}
+// class Users {
+//     constructor(name, email) {
+//         this.name = name;
+//         this.email = email;
+//     }
+// }
 
-let userSample = new Users("jules", "julesmoreno@gmail.com")
+// let userSample = new Users("jules", "julesmoreno@gmail.com")
 
-console.log(userSample)
+// console.log(userSample)
 
-class Person {
-    constructor(name, age, height) {
-        this.name = name;
-        this.age = age;
-        this.height = height;
-    }
-}
+// class Person {
+//     constructor(name, age, height) {
+//         this.name = name;
+//         this.age = age;
+//         this.height = height;
+//     }
+// }
 
-let person1 = new Person("J7", 18, `5'4 or 6'3 or 171cm`)
+// let person1 = new Person("J7", 18, `5'4 or 6'3 or 171cm`)
 
-console.log(person1)
+// console.log(person1)
 
-class Rectangle {
-    constructor(height, width, color) {
-        this.height = height;
-        this.width = width; 
-        this.colors = color;
-       let message = function() {
-            console.log("rectangle has been created")
-        };
-        message();
-    }
+// class Rectangle {
+//     constructor(height, width, color) {
+//         this.height = height;
+//         this.width = width; 
+//         this.colors = color;
+//        let message = function() {
+//             console.log("rectangle has been created")
+//         };
+//         message();
+//     }
 
-    area() {
-        return `The area is ${this.height * this.width}`
-    }
+//     area() {
+//         return `The area is ${this.height * this.width}`
+//     }
 
-    printDescription() {
-        return `The color is ${this.colors} and the height and the width is ${this.height}, ${this.width}`
-    }
-}
+//     printDescription() {
+//         return `The color is ${this.colors} and the height and the width is ${this.height}, ${this.width}`
+//     }
+// }
 
-let rectangle1 = new Rectangle(3, 5, "yellow")
+// let rectangle1 = new Rectangle(3, 5, "yellow")
 
-console.log(rectangle1.colors)
-console.log(rectangle1.area())
-console.log(rectangle1.printDescription())
+// console.log(rectangle1.colors)
+// console.log(rectangle1.area())
+// console.log(rectangle1.printDescription())
 
 // class square {
 //     constructor(width) {
@@ -224,50 +224,150 @@ console.log(rectangle1.printDescription())
 
 // console.log(box1.hit)
 
-class Triple {
-    static customName = "Tripler";
-    static descripttion = "I triple something";
-    static calculate(n = 1) {
-        return n * 3
+// class Triple {
+//     static customName = "Tripler";
+//     static descripttion = "I triple something";
+//     static calculate(n = 1) {
+//         return n * 3
+//     }
+
+//     // for instance
+//     calc(n=1) {
+//         return n * 3
+//     }
+// }
+
+// class SquaredTriple extends Triple {
+//     static longDescription;
+//     static descripttion = "I square the triple number you provide";
+//     static calculate(n) {
+//         return super.calculate(n) * super.calculate(n);
+//     }
+// }
+
+// console.log(Triple.descripttion)
+// console.log(Triple.calculate())
+// console.log(Triple.calculate(3))
+
+// console.log(SquaredTriple.customName)
+// console.log(SquaredTriple.descripttion)
+// console.log(SquaredTriple.calculate())
+
+// let tp = new Triple()
+// // console.log(tp.calculate())
+// console.log(tp.calc(5))
+
+// function videos(title) {
+//     this.title = title
+//     return this
+// }
+
+// console.log(videos("red dot"))
+// console.log(this.title)
+// this.title = "new Moves"
+// console.log(this.title)
+
+// class Person {
+//     constructor(name, age, id) {
+//         this.name = name;
+//         this.age = age;
+//         this.id = id;
+//     }
+
+//     ageA(birth) {
+//         this.age = 2022 - birth
+//     }
+
+//     static autoAge(birth) {
+//         return this.age = 2022 - birth;
+//     }
+
+//     static autoage = this.autoAge(1997)
+// }
+
+// let person1 = new Person("Jules", null, 3120119);
+
+// console.log(person1)
+// console.log(person1.age)
+
+// person1.ageA(1996);
+// console.log(person1.age)
+
+// console.log(Person.autoAge(1996))
+
+// console.log(Person.autoage)
+
+// class Person {
+//     constructor(name, age, add) {
+//         this.name = name;
+//         this.age = age;
+//         this.add = add;
+//     }
+
+//     static eyeColor = "black";
+// }
+
+// class Worker extends Person {
+//     constructor(name, age, add, occupation) {
+//         super(name, age);
+//         this.occupation = occupation;
+//         this.add = add
+//     }
+
+//     static eye() {
+//         return super.eyeColor
+//     }
+
+//     get colorOfEye() {
+//         return Worker.eye()
+//     }
+
+//     static eyeColor() {
+//         return super.eyeColor
+//     }
+// }
+
+
+// let worker1 = new Worker("Jules", 26, "manila", "none")
+
+// console.log(worker1)
+
+// console.log(worker1.colorOfEye)
+// console.log(Worker.eyeColor())
+
+class CallSomething {
+    constructor() {
+        console.log(this.constructor.callSomething)
+        console.log(this.constructor.staticMethod())
+        console.log(CallSomething.callSomething)
+        console.log(CallSomething.staticMethod())
+        console.log(this.constructor)
+
+        this.greet = "hello"
     }
 
-    // for instance
-    calc(n=1) {
-        return n * 3
-    }
+    static callSomething = "something is stored";
+    static staticMethod() {
+        return "static Method has been called"
+    } 
 }
 
-class SquaredTriple extends Triple {
-    static longDescription;
-    static descripttion = "I square the triple number you provide";
-    static calculate(n) {
-        return super.calculate(n) * super.calculate(n);
-    }
-}
-
-console.log(Triple.descripttion)
-console.log(Triple.calculate())
-console.log(Triple.calculate(3))
-
-console.log(SquaredTriple.customName)
-console.log(SquaredTriple.descripttion)
-console.log(SquaredTriple.calculate())
-
-let tp = new Triple()
-// console.log(tp.calculate())
-console.log(tp.calc(5))
+let something = new CallSomething()
+console.log(something.greet)
 
 
-
+// extends and super
+// static
 
 
 // last topic
-// https://www.youtube.com/watch?v=CwAU1wYeHiM
-// https://www.youtube.com/watch?v=hy-C4NY7A_8&list=PL4cUxeGkcC9i5yvDkJgt60vNVWffpblB7&index=6
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/static
 // https://www.youtube.com/watch?v=2ZphE5HcQPQ
+// https://www.youtube.com/watch?v=hy-C4NY7A_8&list=PL4cUxeGkcC9i5yvDkJgt60vNVWffpblB7&index=6
 
 // 'this' review => https://www.youtube.com/watch?v=gvicrj31JOM
 // classes => https://www.youtube.com/watch?v=2ZphE5HcQPQ
+// extends and super => https://www.youtube.com/watch?v=CwAU1wYeHiM 
 
 // encapsulation
 // study abstraction in oop
