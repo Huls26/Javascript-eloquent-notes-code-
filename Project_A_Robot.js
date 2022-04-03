5e501db8d47661a6675b289ad12396ee6960a417
Jules <julesmoreno66@gmail.com>
2022-04-03 16:42:25 +0800

    object.create

--- Project_A_Robot.js
+++ Project_A_Robot.js
@@ -1,3 +1,74 @@
+// ======= object.create() =========
+// let information = {
+//     printInfo() {
+//         console.log(this.name, this.age)
+//     }
+// }
+
+// let person1 = Object.create(information);
+// person1.name = "Jules";
+// person1.age = 26;
+
+// person1.printInfo()
+// console.log(Object.getPrototypeOf(person1))
+
+class Information {
+    constructor(name, age, sound) {
+        this.name = name;
+        this.age = age;
+        this.salary = 5000000;
+        this.sound = sound;
+    }
+
+    printSalary() {
+        return this.salary
+    }
+}
+
+let persona1 = new Information("J7", 23);
+
+persona1.sound = "hello"
+console.log(persona1)
+
+
+// add a new properties using object.create
+let addFeatures = Object.create(persona1);
+// addFeatures.printInfo = function() {
+//     return `${this.name}, ${this.age}`
+// }
+
+// add sound constructor
+addFeatures.sound = "meow";
+console.log(addFeatures)
+
+// console.log(addFeatures.printInfo())
+// console.log(addFeatures)
+
+// adding a prototype
+// Information.prototype.print = function() {
+//     return this.name
+// }
+
+// testing prototype in each object
+// console.log(persona1.print())
+// console.log(addFeatures.print())
+// console.log(Information.prototype)
+
+// adding a property
+addFeatures.printSalary = "10000000"
+
+console.log(addFeatures.printSalary)
+// to called the prototype of information
+console.log(Information.prototype.printSalary.call(addFeatures))
+
+// loop over the prototype
+for (let element in Object.getOwnPropertyNames(Object.getPrototypeOf(persona1))) {
+    console.log(Object.getOwnPropertyNames(Object.getPrototypeOf(persona1))[element])
+}
+
+// console.log({hello: "world"}.includes("hello"))
+
+// ======= MeadowField =======
 var roads = [
     "Alice's House-Bob's House",   "Alice's House-Cabin",
     "Alice's House-Post Office",   "Bob's House-Town Hall",
@@ -27,22 +98,11 @@ function buildGraph(roads) {
     return graph
 }
 
-console.log(buildGraph(roads))
-
+// console.log(buildGraph(roads))
 // Alice's House: ["Bob's House", 'Cabin', 'Post Office']
 
-let information = {
-    printInfo() {
-        console.log(this.name, this.age)
-    }
-}
 
-let person1 = Object.create(information);
-person1.name = "Jules";
-person1.age = 26;
 
-person1.printInfo()
-console.log(Object.getPrototypeOf(person1))
 
 
 // last topic
