a20435655c2ea28980f57a0f7d768797d1a66729
Jules <julesmoreno66@gmail.com>
2022-04-26 10:45:45 +0800

    Regular expressions

--- Regular_Expressions.js
+++ Regular_Expressions.js
@@ -12,5 +12,29 @@ let re1 = new RegExp("abc"), re2 = /abc/;
 // → false
 
 // Sets of character
-console.log(/[0-9]/.test("in 1992"))
+console.log(/[0-9]/.test("in 1992"));
 console.log(/\d/.test("in 1992"));
+console.log(/i/.test("in 1992"));
+console.log(/les/.test("jules"));
+
+// Repeating parts of a pattern
+console.log(/'\d+'/.test("'123'"));
+// → true
+console.log(/'\d+'/.test("''"));
+// → false
+console.log(/'\d*'/.test("'123'"));
+// → true
+console.log(/'\d*'/.test("''"));
+// → true
+
+console.log(/[0-9]{3}/.test(326))
+console.log(/[a-z]{3}/.test("aba326"))
+console.log(/[a-z]{3,5}/.test("asdasaas"))
+
+// The Date class
+console.log(new Date())
+
+// boundaries
+console.log(/\bt\b/.exec("tweet the ocean"))
+/// Review Regular expressions
+// https://www.youtube.com/watch?v=Se6Oxyl03xE
