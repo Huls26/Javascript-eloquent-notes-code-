e8e4fdc23e45c787a7bcb781dddb8aedebb1935a
Jules <julesmoreno66@gmail.com>
2022-05-02 15:51:57 +0800

    asynchronous programming callbacks

---
+++ Asynchronous_programming.js
@@ -0,0 +1,8 @@
+// Callbacks
+setTimeout(() => {console.log("hello")}, 1000)
+
+// Promises
+
+// last topic 
+// https://www.youtube.com/watch?v=ZcQyJ-gxke0&list=PL4cUxeGkcC9jx2TTZk3IGWKSbtugYdrlu
+// https://www.youtube.com/watch?v=ZYb_ZU8LNxs
