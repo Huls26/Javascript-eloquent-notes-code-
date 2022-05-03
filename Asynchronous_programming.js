b1d7583efe2dfe7b5ec38dd5fb31b9d0b06bb454
Jules <julesmoreno66@gmail.com>
2022-05-03 20:59:42 +0800

    creating promises

--- Asynchronous_programming.js
+++ Asynchronous_programming.js
@@ -42,20 +42,19 @@ function production() {
 
 // Promises
 
-
 // let createSomething = new Promise((resolve, reject) => {
 //     console.log("start here")
 //     resolve()
 // })
 
-function success() {
-    console.log("success")
-    // throw new Error("something went wrong")
-}
+// function success() {
+//     console.log("success")
+//     // throw new Error("something went wrong")
+// }
 
-function fail() {
-    console.log("fail")
-}
+// function fail() {
+//     console.log("fail")
+// }
 
 // let promise = createSomething.then(success, fail).catch(() => {console.log("error")})
 // .then(() => console.log("run this code"))
@@ -87,62 +86,141 @@ function fail() {
 // wait(10*1000).then(() => saySomething("10 seconds")).catch(failureCallback);
 
 // =========== My own Promises =================
+// class MyPromise {
+//     constructor(callback) {
+//         this.fullfilled = null;
+//         this.reject = null;
+//         callback((message) => {
+//             this.fullfilled = message
+//         }, (message) => {this.reject = message} )
+
+//     }
+
+//     then(callback) {
+//      return new MyPromise((resolve, reject) => {
+//          if (callback(this.fullfilled)) {
+//             resolve(this.fullfilled)
+//          } else {
+//             reject("something went wrong")
+//          }
+//      })
+        
+//     }
+
+//     catch(callback) {
+//         callback(this.reject)
+//     }
+// }
+
+// let p1 = new MyPromise((resolve, reject) => {
+//     let a = 1 + 1;
+
+//     if (a === 2) {
+//         resolve("success")
+//     } else {
+//         reject("fail")
+//     }
+
+// })
+
+// let p2 = new MyPromise((resolve, reject) => {
+//     let a = 1 + 12;
+
+//     if (a === 2) {
+//         resolve("success")
+//     } else {
+//         reject("fail")
+//     }
+// })
+
+// if (p1.fullfilled === "success") {
+//     console.log("passed for resolve")
+// }
+
+// p1.then(message => {
+//     if (message === "success") {
+//         console.log("Passed 1")
+//     }
+// })
+
+
+// p2.catch(error => {
+//     if (error === "fail") {
+//         console.log("passed 2")
+//     }
+// })
+
+// function failureCallback() {
+//    console.log("something went wrong")
+// }
+
+// const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+// wait(10*1000).then(() => saySomething("10 seconds")).catch(failureCallback);
+
+// class NewPromise {
+//     constructor(callback) {
+//         this.fullfilled = null
+//         callback((message) => {this.fullfilled = message})
+//     }
+
+//     then(callback) {
+//         callback(this.fullfilled)
+//     }
+// }
+
+
+// let testString = "foo";
+
+// let promise12 = new NewPromise((resolve) => {
+//     // setTimeout(() => resolve(testString), 1000)
+//     resolve(testString)
+// })
+
+// console.log(promise12)
+
+// promise12.then(string => {
+//     if (string === "foo") {
+//         console.log("Passed")
+//     }
+// })
 class MyPromise {
     constructor(callback) {
         this.fullfilled = null;
-        this.reject = null;
-        callback((message) => {
-            this.fullfilled = message
-        }, (message) => {this.reject = message} )
+    }
 
+    static resolve(value) {
+        let create = new MyPromise(value);
+        create.store(value);
+        return create
     }
 
     then(callback) {
         callback(this.fullfilled)
     }
 
-    catch(callback) {
-        callback(this.reject)
+    store(value) {
+        this.fullfilled = value;
     }
 }
 
-let p1 = new MyPromise((resolve, reject) => {
-    let a = 1 + 1;
+let fifteen = MyPromise.resolve(15);
 
-    if (a === 2) {
-        resolve("success")
-    } else {
-        reject("fail")
-    }
-
-})
-
-let p2 = new MyPromise((resolve, reject) => {
-    let a = 1 + 12;
-
-    if (a === 2) {
-        resolve("success")
-    } else {
-        reject("fail")
-    }
-})
-
-if (p1.fullfilled === "success") {
-    console.log("passed for resolve")
+if (fifteen.fullfilled === 15) {
+    console.log("Passed")
+} else {
+    console.log("store to fullfilled")
 }
 
-p1.then(message => {
-    if (message === "success") {
-        console.log("Passed 1")
+fifteen.then(value => {
+    if (value === 15) {
+        console.log("Passed")
+    } else {
+        console.log("the value does not restore to fullfilled")
     }
 })
+// fifteen.then(value => console.log("I got " + value))
 
-
-p2.catch(error => {
-    if (error === "fail") {
-        console.log("passed 2")
-    }
-})
 
 
 
