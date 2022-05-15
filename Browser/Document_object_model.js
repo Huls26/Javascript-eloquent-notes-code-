11f65b760b764638fd07c566859e853e0a95bd0f
Jules <julesmoreno66@gmail.com>
2022-05-15 20:46:08 +0800

    Document Object Model

---
+++ Browser/Document_object_model.js
@@ -0,0 +1,17 @@
+function talksAbout(node, string) {
+    if (node.nodeType == Node.ELEMENT_NODE) {
+      for (let child of node.childNodes) {
+        if (talksAbout(child, string)) {
+          return true;
+        }
+      }
+      return false;
+    } else if (node.nodeType == Node.TEXT_NODE) {
+      return node.nodeValue.indexOf(string) > -1;
+    }
+}
+  
+console.log(talksAbout(document.body, "book"));
+  // → true
+console.log(document.body)
+console.log(Node.ELEMENT_NODE)
