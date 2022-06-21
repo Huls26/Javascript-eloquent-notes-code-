18831eae0626bfe7aff2051188dedc3a74de3198
Jules <julesmoreno66@gmail.com>
2022-06-21 16:34:14 +0800

    pet a cat

--- Browser/canvas/games/petACat.js
+++ Browser/canvas/games/petACat.js
@@ -1,29 +1,53 @@
-let canvas = document.querySelector("canvas");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+"use script"
 
-let c = canvas.getContext("2d");
+// container
+let container = document.createElement("div");
+container.id = "container";
+container.style.width = `${window.innerWidth}px`;
+container.style.height = `${window.innerHeight}px`;
+container.style.border = "solid 3px black";
+container.style.position = "absolute";
+document.body.appendChild(container);
 
-let cats = [];
-function make_base(x, y) {
-    const ship = new Image();
-    ship.src = "../../../../../img/cat.jpeg"
-    ship.onload = function() {
-        c.drawImage(ship, x, y, 100, 100);
-    }
-    cats.push(ship)
+// img
+function img(x, y) {
+    let createImg = document.createElement("img");
+    createImg.src = "../../../../../img/cat.jpeg"
+    createImg.style.top = `${y}px`;
+    createImg.style.left = `${x}px`
+    container.appendChild(createImg);
 }
 
+// total cat
 let totalCat = 0
-while (totalCat < 100) {
-    let x = Math.floor(Math.random() * (window.innerWidth - 150)) + 5;
-    let y = Math.floor(Math.random() * (window.innerHeight - 150)) + 5;
-    cats.push(make_base(x, y))
-    totalCat++
+let totalScore = 0;
+
+// create 100 cat
+function cats() {
+    setInterval(() => {
+        let x = Math.floor(Math.random() * (container.clientWidth - 125)) + 10;
+        let y = Math.floor(Math.random() * (container.clientHeight - 125)) + 10;
+        if (totalCat <= 100) {
+            img(x, y)
+            totalCat++
+        }
+    }, 1000)
 }
 
+cats();
 
-canvas.addEventListener("click", event => {
-    // c.clearRect(0, 0, event.pageX, event.pageY);
-    console.log(event.target)
+let score = document.querySelector("span");
+score.innerText = totalScore;
+
+window.addEventListener("click", event => {
+    if (event.target.src) {
+        container.removeChild(event.target)
+        totalScore++
+        score.innerText = totalScore;
+        totalCat--
+        if (totalScore === 50) {
+            alert("great you pet 10 cats") 
+        }
+    } 
 })
+
