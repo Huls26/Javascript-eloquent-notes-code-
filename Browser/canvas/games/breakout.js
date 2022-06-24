537cfab6f34ce33a135390b4ec5ef4df42a52459
Jules <julesmoreno66@gmail.com>
2022-06-24 16:20:08 +0800

    breakout bounce ball

--- Browser/canvas/games/breakout.js
+++ Browser/canvas/games/breakout.js
@@ -3,7 +3,6 @@ document.body.style.display = "flex";
 document.body.style.justifyContent = "center";
 
 let canvas = document.querySelector("canvas");
-
 canvas.style.marginTop = "20px";
 canvas.width = 320;
 canvas.height = 500;
@@ -34,6 +33,12 @@ class Board {
     }
 
     update(x) {
+        if (mouse.x <= 0) {
+            mouse.x = 0;
+        } else if (mouse.x >= rs) {
+            mouse.x = rs;
+        }
+
         let board = new Board(x);
         board.draw()
     }
@@ -44,14 +49,15 @@ let rs = canvas.width - 90;
 
 // ball
 class Ball {
-    constructor(x, y, radius) {
+    constructor(x, y, radius, dx, dy) {
         this.x = x;
         this.y = y;
-        this.dx = 4;
-        this.dy = 4
+        this.dx = dx;
+        this.dy = dy;
         this.radius = radius;
     }
 
+    // draw the ball
     draw() {
         c.beginPath()
         c.arc(this.x, this.y, this.radius, 0, Math.PI *2, false)
@@ -60,13 +66,36 @@ class Ball {
         c.closePath()
     }
 
-    update(x) {
-        let ball = new Ball (this.x, this.y, this.radius)
+    update() {
+        // stick the ball at start
+        let x
+        let y
+        if (!this.dx) {
+            x = mouse.x + 45;
+            y =  460 - 9
+            
+            if (mouse.x <= 0) {
+                x = 45 ;
+            } else if (mouse.x >= rs) {
+                x = rs + 45;
+            }
+        } else {
+            x = this.x;
+            y = this.y
+        }
+
+        // for bounce the wall
+        if (this.x >= canvas.width || this.x <= 0) {
+            this.dx = -this.dx;
+        } else if (this.y >= canvas.height || this.y <= 0) {
+            this.dy = -this.dy;
+        }
+
+        // update ball
+        this.x += this.dx;
+        this.y += this.dy;
+        let ball = new Ball(x, y, this.radius, this.dx, this.dy)
         ball.draw()
-    }
-
-    stick() {
-
     }
 }
 
@@ -75,32 +104,33 @@ class Ball {
 let board = new Board(mouse.x);
 board.draw()
 
-//ball
-let ball = new Ball(mouse.x + 45, 460 - 9, 9);
+// ball
+let speed = 4
+let radius = 9
+let ball = new Ball(mouse.x + 45, 460 - 9, radius, 0, 0);
 ball.draw()
+ball.update()
 
+// event
 // mouse event
 canvas.addEventListener("mousemove", event => {
     mouse.x = event.pageX - (canvas.width-45);
 })
 
+// start stick ball
 canvas.addEventListener("click", event => {
+    if (event.button === 0) {
+        ball.dx = speed;
+        ball.dy = speed;
+    }
 })
-// remove this event after click or use
-
-// removeEventListener
 
 // animate
 function animate() {
     c.clearRect(0, 0, canvas.width, canvas.height)
-    if (mouse.x <= 0) {
-        mouse.x = 0;
-    } else if (mouse.x >= rs) {
-        mouse.x = rs;
-    }
-
-    ball.update()
+  
     c.fillStyle = "blue";
+    ball.update()
     c.fillStyle = "Black";
     board.update(mouse.x)
     requestAnimationFrame(animate);
