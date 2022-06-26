5fa1477f4489519e00cf9f3c9b5e283e0a593f33
Jules <julesmoreno66@gmail.com>
2022-06-26 10:43:11 +0800

    breakout

--- Browser/canvas/games/breakout.js
+++ Browser/canvas/games/breakout.js
@@ -9,6 +9,8 @@ canvas.height = 500;
 
 let c = canvas.getContext("2d");
 
+
+
 // mouse
 let mouse = {
     x: canvas.width / 2 - 45,
@@ -70,6 +72,8 @@ class Ball {
         // stick the ball at start
         let x
         let y
+        this.x += this.dx;
+        this.y += this.dy;
         if (!this.dx) {
             x = mouse.x + 45;
             y =  460 - 9
@@ -85,17 +89,45 @@ class Ball {
         }
 
         // for bounce the wall
-        if (this.x >= canvas.width || this.x <= 0) {
+        if (this.x >= canvas.width - 7 || this.x <= 7) {
             this.dx = -this.dx;
-        } else if (this.y >= canvas.height || this.y <= 0) {
+        } else if (this.y >= canvas.height - 7 || this.y <= 7) {
             this.dy = -this.dy;
         }
 
+        // bounce to board
+        if ((this.x >= mouse.x && this.x <= mouse.x + 90) && this.y >= 460 - 6) {
+            this.dy = -this.dy;
+        } 
+        // else if (this.x === mouse.x && (this.y >= 460 - 6 && this.y <= 460 + 15)) {
+        //     this.dy = -this.dy;
+        // }
+
         // update ball
-        this.x += this.dx;
-        this.y += this.dy;
         let ball = new Ball(x, y, this.radius, this.dx, this.dy)
         ball.draw()
+    }
+}
+
+// rect
+class Rect {
+    constructor(x) {
+        this.x = x;
+        this.y = 460;
+        this.width = 90; 
+        this.height = 15;
+    }
+
+    draw() {
+        c.beginPath();
+        c.fillRect(mouse.x, 460, 90, 15)
+        c.fillStyle = "red";
+        c.closePath()
+    }
+
+    update(x) {
+        let board = new Board(x);
+        board.draw()
     }
 }
 
@@ -105,11 +137,11 @@ let board = new Board(mouse.x);
 board.draw()
 
 // ball
-let speed = 4
-let radius = 9
+let speed = -1;
+let radius = 9;
 let ball = new Ball(mouse.x + 45, 460 - 9, radius, 0, 0);
-ball.draw()
-ball.update()
+// ball.draw()
+// ball.update()
 
 // event
 // mouse event
@@ -122,13 +154,19 @@ canvas.addEventListener("click", event => {
     if (event.button === 0) {
         ball.dx = speed;
         ball.dy = speed;
+
+        ball.x = (event.pageX - (canvas.width -45)) + 45; 
+        if (event.pageX - (canvas.width-45) <= 0) {
+            ball.x = 45;
+        } else if (event.pageX - (canvas.width-45) >= rs) {
+            ball.x = canvas.width - 45;
+        }
     }
 })
 
 // animate
 function animate() {
     c.clearRect(0, 0, canvas.width, canvas.height)
-  
     c.fillStyle = "blue";
     ball.update()
     c.fillStyle = "Black";
