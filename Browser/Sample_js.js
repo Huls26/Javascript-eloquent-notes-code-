9fce8821c8ffb9a912f41e3a5fc7ab6410b07c92
Jules <julesmoreno66@gmail.com>
2022-07-05 20:38:24 +0800

    draw bars

--- Browser/Sample_js.js
+++ Browser/Sample_js.js
@@ -62,7 +62,7 @@ function removeDuplicates(string) {
     console.log(singleW.join(""))
 }
 
-removeDuplicates("bookkeeper")
+// removeDuplicates("bookkeeper")
 // "bokeper"
 
 // removeDuplicates("b")
@@ -83,6 +83,72 @@ function countUnique(array) {
     console.log(unique.length)
 }
 
-countUnique([4, 7, 4, 9, 2, 7, 8, 7, 4, 7, 2])
-countUnique([45])
-countUnique([5, 5, 5, 5])
+// countUnique([4, 7, 4, 9, 2, 7, 8, 7, 4, 7, 2])
+// countUnique([45])
+// countUnique([5, 5, 5, 5])
+
+// draw bars
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let c = canvas.getContext("2d");
+
+function drawLine(beginPath, endPath) {
+    c.beginPath();
+    c.moveTo(beginPath.x, beginPath.y);
+    c.lineTo(endPath.x, endPath.y);
+    c.stroke();
+}
+
+function bars(width, height, n) {
+    let space = width / n;
+    let value = 5;
+    let start = 5;
+    let base = 5
+    
+    // column
+    for (let i = 0; i < n; i++) {
+          // stroke point
+        let beginPath = {
+            x: value,
+            y: base,
+        }
+        
+        let endPath = {
+            x: value,
+            y: height,
+        }
+        c.strokeStyle = "black";
+        drawLine(beginPath, endPath)
+        value += space;
+    }
+
+    value -= space; 
+    
+    // row
+    for (let i = 0; i < 2; i++) {
+        let beginPath = {
+            x: base,
+            y: start,
+        }
+        
+        let endPath = {
+            x: value,
+            y: start,
+        }
+
+        c.strokeStyle = "blue"
+        drawLine(beginPath, endPath)
+        start = height;
+    }
+}
+
+bars(300, 200, 10)
+
+// exercises
+// https://web.stanford.edu/class/cs106a-8/sum-assn/homework-5-quilt.html
+// https://web.stanford.edu/class/archive/cs/cs106a/cs106a.1178/resources/midterm/PracticeMidterm.pdf
+// https://web.stanford.edu/class/archive/cs/cs106a/cs106a.1226/handouts/12-assignment5.html#part-2-word-guessing
+
+// http://web.stanford.edu/class/archive/cs/cs106a/cs106a.1178/resources/midterm/MidtermQuestionBooklet.pdf
