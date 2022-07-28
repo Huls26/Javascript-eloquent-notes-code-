455b94aec98fb2565c074bb3c2db2ab083a7be6a
Jules <julesmoreno66@gmail.com>
2022-07-28 15:53:28 +0800

    warhol images

--- cs106A/assingment3/Lists_and _Images.js
+++ cs106A/assingment3/Lists_and _Images.js
@@ -7,9 +7,9 @@ function greaterThan(num, array) {
     console.log(output)
 }
 
-greaterThan(6, [20, 6, 12, -3, 14]);
+// greaterThan(6, [20, 6, 12, -3, 14]);
 
-greaterThan(0, []);
+// greaterThan(0, []);
 
 // 2. Reading values into a list and removing duplicates
 
@@ -73,35 +73,97 @@ let ctx = canvas.getContext("2d");
 canvas.width = 700;
 canvas.height = 400;
 
-const image = new Image();
-image.src = "rodeo_chediski_satellite.jpg";
+// const image = new Image();
+// // image.src = "rodeo_chediski_satellite.jpg";
+// image.src = "08-image1.png"
 
-image.addEventListener('load', () => {
-    ctx.drawImage(image, 0, 0, 233, 190);
+// image.addEventListener('load', () => {
+//     ctx.drawImage(image, 0, 0, 233, 190);
   
-    const imageData = ctx.getImageData(0, 0, 233, 190);
+//     const imageData = ctx.getImageData(0, 0, 233, 190);
 
-    let second = imageData.data;
-    let intensityThreshold = 1.14;
+//     let second = imageData.data;
+//     // let intensityThreshold = 1.14;
+//     let intensityThreshold = 1.05;
 
-    for (let i = 0; i < second.length; i += 4) {
-        let total = second[i] + second[i + 1] + second[i + 2];
-        let aveThreshold = (total / 3) * intensityThreshold
+//     for (let i = 0; i < second.length; i += 4) {
+//         let total = second[i] + second[i + 1] + second[i + 2];
+//         let aveThreshold = (total / 3) * intensityThreshold
 
-        if (second[i] >= aveThreshold ) {
-            second[i] = 255;
-            second[i + 1] = 0;
-            second[i + 2] = 0;
-        } else {
-            second[i] = aveThreshold;
-            second[i + 1] = aveThreshold;
-            second[i + 2] = aveThreshold;
+//         if (second[i] >= aveThreshold ) {
+//             second[i] = 255;
+//             second[i + 1] = 0;
+//             second[i + 2] = 0;
+//         } else {
+//             second[i] = aveThreshold;
+//             second[i + 1] = aveThreshold;
+//             second[i + 2] = aveThreshold;
+//         }
+//     }
+
+//     ctx.putImageData(imageData, 240, 0)
+// });
+
+// 2. Warhol Images
+// a. Task: Create a filter for a single image
+
+function createFilteredImage(redScale, greenScale, blueScale, x, y) {
+    let width
+    let height
+
+    const image = new Image();
+    image.src = "08-image3.png"
+
+    width = image.width;
+    height = image.height; 
+
+    image.addEventListener("load", () => {
+        ctx.drawImage(image, x, y, width, height)
+
+        const imageData = ctx.getImageData(x, y, width, height);
+
+        const data = imageData.data;
+
+        for (let i = 0; i < data.length; i += 4) { 
+            let [red, green, blue] = [data[i], data[i + 1], data[i + 2]];
+
+            data[i] = red * redScale;
+            data[i + 1] =  green * greenScale;
+            data[i + 2] = blue * blueScale;
+        }
+
+        ctx.putImageData(imageData, x, y);
+    })
+
+    return [width, height]
+}
+
+// b. Task: Create a Warhol-style image
+
+function makeWarhol() {
+    let row = 2;
+    let column = 3;
+    let x = 0;
+    let y = 0;
+    let [r, g, b] = [0, 0, 0];
+
+    for (let i = 0; i < row; i++) {
+        for (let k = 0; k < column; k++) {
+            let [width, height] = createFilteredImage(1.5 + r, 0.7 + g, 1.5 + b, x, y);
+        
+            x = x + width;
+            r -= .26;
+            g -= .0001;
+            if (k === column -1) {
+                b += .326;
+                x = 0;
+                y = y + height
+            }
         }
     }
+}
 
-    ctx.putImageData(imageData, 240, 0)
-});
-
+makeWarhol()
 
 // Assignment #3: Lists and Images
 // => https://web.stanford.edu/class/archive/cs/cs106a/cs106a.1226/handouts/08-assignment3.html
