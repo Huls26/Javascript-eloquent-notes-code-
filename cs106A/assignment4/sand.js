691194dcf5d56ad1c8475cc9ce535f705977d07c
Jules <julesmoreno66@gmail.com>
2022-08-04 11:57:46 +0800

    gravity

--- cs106A/assignment4/sand.js
+++ cs106A/assignment4/sand.js
@@ -5,20 +5,55 @@ function doMove(grid, x1, y1, x2, y2) {
     let old = grid[y1][x1];
     grid[y1][x1] = null;
     grid[y2][x2] = old;
+   
     
     return grid
 }
 
 // 2. Task: Checking legal moves
 function checkMove(grid, x1, y1, x2, y2) {
+    // down
     if (grid[y2][x2]) {
         return false
     }
-    if (!grid[x2] || !grid[y2]) {
+
+    if (!grid[y2]) {
         return false
     }
 
+    if (grid[y1][x1]) {
+        return true
+    }
+
     return true
+}
+
+// 3. Task: Gravity
+function doGravity(grid, x, y) {
+    if (!grid[y][x] || y === grid.length -1) {
+        return grid
+    }
+
+    // bottom
+    if (checkMove(grid, x, y, x, y + 1)) {
+        doMove(grid, x, y, x, y + 1)
+    }
+
+    // down left
+    if (checkMove(grid, x, y, x - 1, y + 1)) {
+        if (!grid[y][x-1]) {
+            doMove(grid, x, y, x - 1, y + 1)
+        }
+    }
+
+    // down right
+    if (checkMove(grid, x, y, x + 1, y + 1)) {
+        if (!grid[y][x + 1]) {
+        doMove(grid, x, y, x + 1, y + 1)
+        }
+    }
+
+    return grid
 }
 
 // test
@@ -32,13 +67,50 @@ function test() {
     // doMove(grid, 2, 0, 2, 1)
 
     // 2. Task: Checking legal moves
-    console.log(checkMove(grid, 1, 0, 0, 0));
-    // true
-    console.log(checkMove(grid, 1, 0, 1, 1));
-    // true
-    console.log(checkMove(grid, 1, 0, 2, 0));
-    // false
-    console.log(checkMove(grid, 1, 0, 2, 0));
+    // console.log(checkMove(grid, 1, 0, 0, 0));
+    // // true
+    // console.log(checkMove(grid, 1, 0, 1, 1));
+    // // true
+    // console.log(checkMove(grid, 1, 0, 2, 0));
+    // // false
+    // console.log(checkMove(grid, 1, 0, 2, 0));
+    // // false
+
+    // 3. Task: Gravity
+    grid = [[None, 's', None], [None, None, None]]
+    console.log(doGravity(grid, 0, 0))
+
+    // >>> # down
+    grid = [[None, 's', None], [None, None, None]];
+    console.log(doGravity(grid, 1, 0));
+    // [[None, None, None], [None, 's', None]]
+
+    // >>> # bottom blocked 
+     grid = [[None, 's', None], ['r', 'r', 'r']]
+    console.log(doGravity(grid, 1, 0))
+    // [[None, 's', None], ['r', 'r', 'r']]
+
+    // >>> # rock-below down-left
+    grid = [[None, 's', None], [None, 'r', None]]
+    console.log(doGravity(grid, 1, 0))
+    // [[None, None, None], ['s', 'r', None]]
+
+    // # sand-below down-right
+    grid = [[None, 's', None], ['s', 's', None]]
+    console.log(doGravity(grid, 1, 0));
+    // [[None, None, None], ['s', 's', 's']]
+
+    // # sand corner: down-right
+    grid = [['s', 's', None], [None, 's', None]]
+    console.log(doGravity(grid, 1, 0));
+    // [['s', None, None], [None, 's', 's']]
+
+    // >>> # at bottom already
+    grid = [[None, None, None], [None, 's', None]]
+    console.log(doGravity(grid, 1, 1))
+    // [[None, None, None], [None, 's', None]]
 }
 
 test()
+
+// https://web.stanford.edu/class/archive/cs/cs106a/cs106a.1226/handouts/11-assignment4.html
