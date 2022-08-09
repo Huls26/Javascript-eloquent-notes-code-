1d7488f34dead5510d5dd68ce17e7aa851f25381
Jules Moreno <julesmoreno66@gmail.com>
2022-08-09 16:14:38 -0700

    add item to word_data

--- cs106A/assignment6/biasBarsData.js
+++ cs106A/assignment6/biasBarsData.js
@@ -1,5 +1,9 @@
 "use strict"
 
+
+const KEY_WOMEN = "W"
+const KEY_MEN = "M"
+
 function convertRatingToIndex(rating) {
     if (rating < 2.5) {
         return 0
@@ -10,6 +14,54 @@ function convertRatingToIndex(rating) {
     }
 }
 
+function addDataForWord(word_data, word, gender, rating) {
+    let defaultDict = {
+                        "W": [0, 0, 0],
+                        "M": [0, 0, 0]
+                    };
+
+    let index = convertRatingToIndex(rating);
+
+    if (word_data[word]) {
+        word_data[word][gender][index]++
+    } else {
+        word_data[word] = defaultDict;
+        word_data[word][gender][index]++;
+    }
+}
+
+function testData() {
+    let word_data = {};
+    addDataForWord(word_data, "good", "M", 5.0);
+    console.log(word_data)
+    // "{'good': {'W': [0, 0, 0], 'M': [0, 0, 1]}}"
+    
+    addDataForWord(word_data, "good", "W", 4.5);
+    console.log(word_data)
+    // {'good': {'W': [0, 0, 1], 'M': [0, 0, 1]}}
+
+    // addDataForWord(word_data, "good", "M", 5.0);
+    // console.log(word_data)
+    // {'good': {'W': [0, 0, 1], 'M': [0, 0, 2]}}
+
+    addDataForWord(word_data, "good", "W", 3.0);
+    console.log(word_data)
+    // {'good': {'W': [0, 1, 1], 'M': [0, 0, 1]}}
+
+    // addDataForWord(word_data, "good", "W", 3.5);
+    // console.log(word_data)
+    // // {'good': {'W': [0, 1, 1], 'M': [0, 0, 1]}}
+
+    // addDataForWord(word_data, "good", "M", 2.5);
+    // console.log(word_data)
+    // // {'good': {'W': [0, 1, 1], 'M': [0, 0, 1]}}
+
+    addDataForWord(word_data, "bad", "M", 1.5);
+    console.log(word_data)  
+    //{'good': {'W': [0, 1, 1], 'M': [0, 0, 1]}, 'bad': {'W': [0, 0, 0], 'M': [1, 0, 0]}}
+}
+
+testData()
 function testRating() {
     if ((convertRatingToIndex(1.0)) === 0) {
         console.log("Passed")
@@ -41,7 +93,6 @@ function testRating() {
         console.log("failed", convertRatingToIndex(4.0))
     }
 
-    
     if ((convertRatingToIndex(5.5)) === 2) {
         console.log("Passed")
     } else {
@@ -49,4 +100,4 @@ function testRating() {
     }
 }
 
-testRating()
+// testRating()
