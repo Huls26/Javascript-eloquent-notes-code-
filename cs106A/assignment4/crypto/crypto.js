08021d515b538f86c1b56650f6b75b06527d6e55
Jules Moreno <julesmoreno66@gmail.com>
2022-08-14 16:26:22 -0700

    never gonna give you up

--- cs106A/assignment4/crypto/crypto.js
+++ cs106A/assignment4/crypto/crypto.js
@@ -2,7 +2,7 @@
 
 const ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-function  computeSlug(key) {
+function computeSlug(key) {
     let split = key.toLowerCase().split("");
     let newA = ALPHABET.slice(0);
     let container = [];
@@ -21,7 +21,80 @@ function  computeSlug(key) {
     return newA
 }
 
-console.log(computeSlug("z"))
-console.log(computeSlug('Bananas!'))
-console.log(computeSlug('Life, Liberty, and'))
-console.log(computeSlug('Zounds!'))
+// console.log(computeSlug("z"))
+// console.log(computeSlug('Bananas!'))
+// console.log(computeSlug('Life, Liberty, and'))
+// console.log(computeSlug('Zounds!'))
+
+function encryptChar(source, slug, ch) {
+    let chL = ch.toLowerCase();
+    let index = source.indexOf(chL);
+    let char =  slug[index]
+
+    if (index !== -1) {
+        if (ch === chL) {
+            return char
+        } else {
+            return char.toUpperCase();
+        }
+    }
+   
+    return ch
+}
+
+const z_slug = computeSlug("z");
+// console.log(encryptChar(ALPHABET, z_slug, 'A'))
+// console.log(encryptChar(ALPHABET, z_slug, 'n'))
+// console.log(encryptChar(ALPHABET, z_slug, 'd'))
+// console.log(encryptChar(ALPHABET, z_slug, '.'))
+// console.log(encryptChar(ALPHABET, z_slug, '\\n'))
+
+function encryptStr(source, slug, s) {
+    let split = s.split("");
+
+    for (let i = 0; i < split.length; i++) {
+        let char = split[i];
+
+        split[i] = encryptChar(source, slug, char);
+    }
+
+    return split.join("")
+}
+
+// console.log(encryptStr(ALPHABET, z_slug, 'And like a thunderbolt he falls.\\n'))
+
+function decryptStr(source, slug, s) {
+    return encryptStr(slug, source, s);
+}
+
+// console.log(decryptStr(ALPHABET, z_slug, 'Zmc khjd z sgtmcdqanks gd ezkkr.\\n'))
+
+
+async function encryptFile(filename, key) {
+    let data = await (await fetch(filename)).text();
+    const alphabet = ALPHABET; 
+    let z_slug = computeSlug(key);
+
+    const encrypted = encryptStr(alphabet, z_slug, data)
+
+    return encrypted
+}
+
+// encryptFile("./crypto_data/the-eagle.txt", 'Bananas')
+
+async function decrypt_file(filename, key) {
+    let data = await (await fetch(filename)).text();
+    const alphabet = ALPHABET; 
+    let z_slug = computeSlug(key);
+
+    const decrypted = decryptStr(alphabet, z_slug, data) 
+
+    console.log(decrypted)
+    return decrypted
+}
+
+// decrypt_file("./crypto_data/independence-crypt.txt", "Bleh");
+
+decrypt_file("./crypto_data/mystery-crypt.txt", "cardinal red");
+
+// https://web.stanford.edu/class/cs106a-8/sum-assn/homework-4-crypto.html
